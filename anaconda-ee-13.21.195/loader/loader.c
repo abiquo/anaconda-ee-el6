2acadc2014fe26b48d93bf617809c4b1730109ef
Abel Boldú <abel.boldu@abiquo.com>
2013-10-30 16:33:46 +0100

    writing enabled netconfig in loader.c

--- anaconda-ee-13.21.195/loader/loader.c
+++ anaconda-ee-13.21.195/loader/loader.c
@@ -2287,10 +2287,10 @@ int main(int argc, char ** argv) {
         logMessage(ERROR, "chooseNetworkInterface failure: %d", i);
     }
     else {
-        logMessage(INFO, "Interface chosen: %d", loaderData->netDev);
+        logMessage(INFO, "Interface chosen: %d", &(loaderData->netDev));
     }
-    devName = loaderData->netDev;
-    strcpy(iface.device, devName);
+
+    strcpy(iface.device, &(loaderData->netDev));
     if ((i = writeEnabledNetInfo(&iface)) != 0) {
         logMessage(ERROR, "writeEnabledNetInfo failure: %d", i);
     }
